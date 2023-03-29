a9a35d697763c2f61d91cb10274b495379d6670f
Annie <115146344+AnnieOhlen@users.noreply.github.com>
2023-03-29 15:28:04 +0200

    JsonConvert
    Update.

--- ShopGeneral/Services/ReportService.cs
+++ ShopGeneral/Services/ReportService.cs
@@ -1,9 +1,13 @@
-﻿using ShopGeneral.Data;
+﻿using HtmlAgilityPack;
+using ShopGeneral.Data;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace ShopGeneral.Services
 {
@@ -11,7 +15,24 @@ namespace ShopGeneral.Services
     {
         public string JsonProductReport(List<Product> products)
         {
-            throw new NotImplementedException();
+            var JsonObject = JsonSerializer.Serialize(JsonProductReport);
+            //return tidigare här uppe
+
+            var json = JObject.Parse(JsonSerializer.Serialize<List<ShopGeneral.Data.Product>>(products));
+            var data = JObject.FromObject(new { Total = products.Count, Skip = 0m, Limit = 0 });
+            var complete = new JObject();
+            complete.Merge(json);
+            complete.Merge(data);
+            return complete.ToString();
+
+            //return JsonObject;
         }
+
+        /*
+          "total": 100,
+          "skip": 0,
+          "limit": 30
+        */
+
     }
 }
