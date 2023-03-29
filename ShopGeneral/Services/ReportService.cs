2a9bd874785e8debe86244f8a3c986e51e6425a7
0h4y <johan.kreivi@gmail.com>
2023-03-29 13:27:58 +0200

    Done

---
+++ ShopGeneral/Services/ReportService.cs
@@ -0,0 +1,17 @@
+﻿using ShopGeneral.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopGeneral.Services
+{
+    internal class ReportService : IReportService
+    {
+        public string JsonProductReport(List<Product> products)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
