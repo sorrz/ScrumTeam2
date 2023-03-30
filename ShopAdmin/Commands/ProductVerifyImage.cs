303752c34cfee04dbe95656b03bb95d6a2056103
Annie <115146344+AnnieOhlen@users.noreply.github.com>
2023-03-30 15:11:44 +0200

    Added ProductVerifyImage class.
    Update.

---
+++ ShopAdmin/Commands/ProductVerifyImage.cs
@@ -0,0 +1,40 @@
+﻿using ShopGeneral.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ShopAdmin.Commands
+{
+    public class ProductVerifyImage : ConsoleAppBase
+    {
+        private readonly IProductService _productService;
+        private readonly IReportService _reportService;
+
+        public ProductVerifyImage(IProductService productService, IReportService reportService)
+        {
+            _productService = productService;
+            _reportService = reportService;
+        }
+
+        public void Export(string to)
+        {
+            var listOfMissingImages = _productService.VerifyProductImages();
+            var report = _reportService.JsonProductReport(listOfMissingImages);
+
+            var folderPath = Path.Combine("outfiles", to);
+            var fullFilePath = Path.Combine(folderPath, DateTime.Now.ToString("yyyyMMdd") + ".txt");
+
+            Directory.CreateDirectory(folderPath);
+
+            using (StreamWriter streamWriter = new StreamWriter(fullFilePath))
+            {
+                streamWriter.Write(report);
+            }
+
+        }
+    }
+}
