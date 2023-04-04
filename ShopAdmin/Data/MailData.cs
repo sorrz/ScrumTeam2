085217418baa9ab217f955e3ebab341501921e3b
0h4y <johan.kreivi@gmail.com>
2023-04-04 12:45:19 +0200

    fix

--- ShopAdmin/Data/MailData.cs
+++ ShopAdmin/Data/MailData.cs
@@ -4,8 +4,8 @@
     {
         // Receiver
         public List<string> To { get; }
-        public List<string> Bcc { get; }
-        public List<string> Cc { get; }
+        public List<string>? Bcc { get; }
+        public List<string>? Cc { get; }
 
         // Sender
         public string? From { get; }
