c520ddac0f8d517799569f4fe2971618f6b5ee57
Dan Stjärnborg <dan.stjarnborg@gmail.com>
2023-04-07 14:47:30 +0200

    hmmmmmm

--- ShopGeneral/Services/ProductService.cs
+++ ShopGeneral/Services/ProductService.cs
@@ -84,12 +84,14 @@ public class ProductService : IProductService
     public List<string> JsonToXml(List<string> JsonInput)
     {
         List<string> xmlString = new();
-        if (JsonInput != null) return null;
+        //if (JsonInput != null) return null;
+        //XmlDocument doc = new();
         foreach (var item in JsonInput)
         {
             XmlDocument doc = (XmlDocument)JsonConvert.DeserializeXmlNode(item);
             var x = doc.InnerXml.ToString();
             xmlString.Add(x);
+            doc.RemoveAll(); // Ska fixas
         }
 
         return xmlString;
