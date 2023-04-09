32d43eb1e4a5acc69410a373d5ff4b86f1e46100
0h4y <johan.kreivi@gmail.com>
2023-04-09 10:32:53 +0200

    RomanAervice Init

---
+++ ShopGeneral/Services/RomanService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopGeneral.Services
+{
+    internal class RomanService : IRomanService
+    {
+        public int ConvertFromRoman(string roman)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ConvertToRoman(int number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
